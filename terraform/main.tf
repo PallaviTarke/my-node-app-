4c5b286c07b1f5f463acdec5375ef890215d5599
PallaviTarke <tarkeshubham2006@gmail.com>
2025-05-28 18:00:42 +0530

    Use Compute Engine default service account for deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,8 +4,8 @@ provider "google" {
 }
 
 # Enable required APIs
-resource "google_project_service" "run_api" {
-  service = "run.googleapis.com"
+resource "google_project_service" "compute_api" {
+  service = "compute.googleapis.com"
   disable_on_destroy = false
 }
 
@@ -14,34 +14,59 @@ resource "google_project_service" "container_registry_api" {
   disable_on_destroy = false
 }
 
-# Cloud Run service
-resource "google_cloud_run_service" "node_app" {
-  name     = var.service_name
-  location = var.region
+# Create a Compute Engine VM
+resource "google_compute_instance" "node_app_vm" {
+  name         = var.vm_name
+  machine_type = "e2-micro" # Free-tier eligible
+  zone         = "${var.region}-a"
 
-  template {
-    spec {
-      containers {
-        image = "gcr.io/${var.project_id}/${var.service_name}:latest"
-      }
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable" # Container-Optimized OS
     }
   }
 
-  traffic {
-    percent         = 100
-    latest_revision = true
+  network_interface {
+    network = "default"
+    access_config {} # Assigns external IP
   }
 
+  metadata = {
+    "gce-container-declaration" = <<-EOT
+    spec:
+      containers:
+      - name: ${var.service_name}
+        image: gcr.io/${var.project_id}/${var.service_name}:latest
+        env:
+        - name: PORT
+          value: "8080"
+      restartPolicy: Always
+    EOT
+  }
+
+  service_account {
+    email  = "428456782983-compute@developer.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
+
+  tags = ["http-server"]
+
   depends_on = [
-    google_project_service.run_api,
+    google_project_service.compute_api,
     google_project_service.container_registry_api
   ]
 }
 
-# Allow unauthenticated access (optional, remove for private service)
-resource "google_cloud_run_service_iam_member" "public_access" {
-  service  = google_cloud_run_service.node_app.name
-  location = google_cloud_run_service.node_app.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+# Firewall rule to allow HTTP traffic
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["http-server"]
 }
